c60f47ac4bbd701ac0a97fef8d0ebb0677055fae
AlexChungA <59289797+AlexChungA@users.noreply.github.com>
2020-09-04 09:52:27 -0500

    algunas interacciones con voz

---
+++ index.py
@@ -0,0 +1,42 @@
+from flask import Flask, render_template, request, redirect, url_for, flash, copy_current_request_context
+import concurrent.futures
+import threading
+import voice_interact as vi
+
+app = Flask(__name__)
+
+app.secret_key = "mysecretkey"
+
+inicio = ["Qué desea hacer", "Ver su calendario", "Leer pdf", "Ver su ubicación"]
+
+def actividades():
+    for text in inicio:
+        vi.engine_speak(text)
+
+@app.route('/acciones')
+def acciones():
+    respuesta = vi.record_audio("Lo escucho")
+    if "calendario" in respuesta:
+        return redirect(url_for('calendario'))
+    elif ("pdf" in respuesta) or ("archivo" in respuesta):
+        return redirect(url_for('archivo'))
+    elif ("mapa" in respuesta) or ("ubicación" in respuesta):
+        return redirect(url_for('mapa'))
+    else:
+        return redirect(url_for('conversar', input = respuesta))
+
+@app.route('/')
+def Index(): 
+    t = threading.Thread(target=actividades)
+    t.start()      
+    return render_template('index.html')
+
+@app.route('/conversa/<input>')
+def conversar(input):
+    t = threading.Thread(target=vi.respond,args=input)
+    t.start()
+    return render_template('conversa.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=False)
--- voice_interact.py
+++ voice_interact.py
@@ -1,25 +1,224 @@
-# -*- coding: utf-8 -*-
-import pyttsx3
-import playsound
-import speech_recognition as sr
+import speech_recognition as sr # recognise speech
+import playsound # to play an audio file
+from gtts import gTTS # google text to speech
+import random
+from time import ctime # get time details
+import webbrowser # open browser
+import ssl
+import certifi
+import time
+import os # to remove created audio files
+import subprocess
+import bs4 as bs
+import urllib.request
+import requests
 
-def speak(text):
-    eng = pyttsx3.init()
-    eng.setProperty("rate",140)
-    eng.setProperty("volume",1.0)
-    listVoices = eng.getProperty("voices")
-    eng.setProperty("voice", listVoices[2].id)
-    eng.say(text)
-    eng.runAndWait()
+class person:
+    name = ''
+    def setName(self, name):
+        self.name = name
 
-def listen():
-    r = sr.Recognizer()
-    palabra = ""
-    with sr.Microphone(device_index = 0) as source:
-        audio = r.listen(source)
+class asis:
+    name = ''
+    def setName(self, name):
+        self.name = name
+
+
+
+def there_exists(terms):
+    for term in terms:
+        if term in voice_data:
+            return True
+
+r = sr.Recognizer() # initialise a recogniser
+# listen for audio and convert it to text:
+def record_audio(ask=""):
+    with sr.Microphone() as source: # microphone as source
+        if ask:
+            engine_speak(ask)
+        audio = r.listen(source, 5, 5)  # listen for the audio via source
+        print("Terminé de escucharte fuerte y claro")
+        voice_data = ''
         try:
-            palabra = r.recognize_google(audio,language="es")
-            print(palabra)
-        except Exception as e:
-            print("Exception: " + str(e))
-    return palabra
+            voice_data = r.recognize_google(audio,language="es-PE")  # convert audio to text
+        except sr.UnknownValueError: # error: recognizer does not understand
+            engine_speak('No te escuché bien')
+        except sr.RequestError:
+            engine_speak('Disculpa, el servidor se cayó') # error: recognizer is not connected
+        print(">>", voice_data.lower()) # print what user said
+        return voice_data.lower()
+
+# get string and make a audio file to be played
+def engine_speak(audio_string):
+    audio_string = str(audio_string)
+    tts = gTTS(text=audio_string, lang='es') # text to speech(voice)
+    r = random.randint(1,20000000)
+    audio_file = 'audio' + str(r) + '.mp3'
+    tts.save(audio_file) # save as mp3
+    playsound.playsound(audio_file) # play the audio file
+    print(asis_obj.name + ":", audio_string) # print what app said
+    os.remove(audio_file) # remove audio file
+
+def respond(voice_data):
+    # 1: greeting
+    for term in ['hey','hola','aló']:
+        if term in voice_data:
+            greetings = ["hola, ¿cómo puedo ayudarte?" + person_obj.name, "hola, ¿qué tal?" + person_obj.name, "Te escucho" + person_obj.name, "¿Te puedo ayudar?" + person_obj.name, "hola" + person_obj.name]
+            greet = greetings[random.randint(0,len(greetings)-1)]
+            engine_speak(greet)
+
+    # 2: name
+    for term in ["cuál es tu nombre","cómo te llamas","dime tu nombre"]:
+        if term in voice_data:
+            if person_obj.name:
+                engine_speak(f"Mi nombre es {asis_obj.name}, gusto en conocerte, {person_obj.name}") #gets users name from voice input
+            else:
+                engine_speak(f"Me llamo {asis_obj.name}. y tú como te llamas?") #incase you haven't provided your name.
+
+    if "mi nombre es" in voice_data:
+        person_name = voice_data.split("es")[-1].strip()
+        engine_speak("está bien, voy a recordar eso " + person_name)
+        person_obj.setName(person_name) # remember name in person object
+    
+    for term in ["cuál es mi nombre","cómo me llamo"]:
+        if term in voice_data:
+            engine_speak("Esa es facil. Tu eres " + person_obj.name)
+    
+    if "ahora te llamas" in voice_data:
+        asis_name = voice_data.split("llamas")[-1].strip()
+        engine_speak("está bien, recordaré que mi nombre ahora es " + asis_name)
+        asis_obj.setName(asis_name) # remember name in asis object
+
+    # 3: greeting
+    for term in ["cómo estas","qué tal la estas pasando"]:
+        if term in voice_data:
+            engine_speak("estoy muy bien, gracias por preguntar " + person_obj.name)
+
+    # 4: time
+    for term in ["qué hora es","dime la hora","qué hora tienes","tienes hora"]:
+        if term in voice_data:
+            time = ctime().split(" ")[3].split(":")[0:2]
+            if time[0] == "00":
+                hours = '12'
+            else:
+                hours = time[0]
+            minutes = time[1]
+            time = "Son las "+ hours + " con " + minutes + "minutos"
+            engine_speak(time)
+
+    # 5: search google
+    for term in ["busca en google","buscar en google"]:
+        if 'youtube' not in voice_data:
+            if term in voice_data:
+                search_term = voice_data.split("google")[-1]
+                url = "https://google.com/search?q=" + search_term
+                webbrowser.get().open(url)
+                engine_speak("Esto es lo que encontre acerca de " + search_term + "en google")
+    
+    if ("quiero saber que es " in voice_data) and ('youtube' not in voice_data):
+        search_term = voice_data.replace("es","")
+        url = "https://google.com/search?q=" + search_term
+        webbrowser.get().open(url)
+        engine_speak("Esto es lo que encontre sobre " + search_term + "en google")
+
+    # 6: search youtube
+    if "youtube" in voice_data:
+        search_term = voice_data.split("busca")[-1]
+        search_term = search_term.replace("en youtube","").replace("busca","")
+        url = "https://www.youtube.com/results?search_query=" + search_term
+        webbrowser.get().open(url)
+        engine_speak("Esto es lo que encontre sobre " + search_term + "en youtube")
+
+     #7: get stock price
+    if "precio del " in voice_data:
+        search_term = voice_data.split("del")[-1]
+        url = "https://google.com/search?q=" + search_term
+        webbrowser.get().open(url)
+        engine_speak("Esto es lo que encontre para " + search_term + " en google")
+    
+    
+     #9 weather
+    if "clima" in voice_data:
+        search_term = voice_data.split("clima")[-1]
+        url = "https://www.google.com/search?sxsrf=ACYBGNSQwMLDByBwdVFIUCbQqya-ET7AAA%3A1578847393212&ei=oUwbXtbXDN-C4-EP-5u82AE&q=weather&oq=weather&gs_l=psy-ab.3..35i39i285i70i256j0i67l4j0i131i67j0i131j0i67l2j0.1630.4591..5475...1.2..2.322.1659.9j5j0j1......0....1..gws-wiz.....10..0i71j35i39j35i362i39._5eSPD47bv8&ved=0ahUKEwiWrJvwwP7mAhVfwTgGHfsNDxsQ4dUDCAs&uact=5"
+        webbrowser.get().open(url)
+        engine_speak("Este es el clima hoy")
+     
+
+     #10 stone paper scisorrs
+    if "jugar" in voice_data:
+        voice_data = record_audio("escoge entre piedra, papel o tijeras")
+        moves=["piedra", "papel", "tijeras"]
+    
+        cmove=random.choice(moves)
+        pmove=voice_data
+        
+
+        engine_speak("elegí " + cmove)
+        engine_speak("Tu elegiste " + pmove)
+
+        if pmove==cmove:
+            engine_speak("hemos empatado")
+        elif pmove== "piedra" and cmove== "tijeras":
+            engine_speak("Tu ganaste")
+        elif pmove== "piedra" and cmove== "papel":
+            engine_speak("Yo gané")
+        elif pmove== "papel" and cmove== "piedra":
+            engine_speak("Tu ganaste")
+        elif pmove== "papel" and cmove== "tijeras":
+            engine_speak("Yo gané")
+        elif pmove== "tijeras" and cmove== "papel":
+            engine_speak("Tu ganaste")
+        elif pmove== "tijeras" and cmove== "piedra":
+            engine_speak("Yo gané")
+
+     #11 toss a coin
+    for term in ["tira","gira","moneda"]:
+        if term in voice_data:
+            moves=["cara", "sello"]   
+            cmove=random.choice(moves)
+            engine_speak("Salió " + cmove)
+
+     #12 calc
+    for term in ["multiplica","+","menos","por","entre","al"]:
+        if term in voice_data:
+            opr = voice_data.split()[1]
+
+            if opr == '+':
+                engine_speak(int(voice_data.split()[0]) + int(voice_data.split()[2]))
+            elif opr == 'menos':
+                engine_speak(int(voice_data.split()[0]) - int(voice_data.split()[2]))
+            elif (opr == 'multiplica') or (opr=='por'):
+                engine_speak(int(voice_data.split()[0]) * int(voice_data.split()[2]))
+            elif opr == 'entre':
+                engine_speak(int(voice_data.split()[0])/int(voice_data.split()[2]))
+            elif opr == 'al':
+                engine_speak(int(voice_data.split()[0]) ** int(voice_data.split()[2]))
+            else:
+                engine_speak("Operador Incorrecto")
+
+    for term in ["salir", "apagar", "adiós","hasta pronto", "chau","hasta luego"]:
+        if term in voice_data:
+            engine_speak("nos vemos")
+            exit()
+
+    # Current city or region
+    if "dónde estoy" in voice_data:
+        Ip_info = requests.get('https://api.ipdata.co?api-key=test').json()
+        loc = Ip_info['region']
+        engine_speak(f"Estás en algún lugar en {loc}")    
+   
+   # Current location as per Google maps
+    if "dime mi ubicación exacta" in voice_data:
+        url = "https://www.google.com/maps/search/Where+am+I+?/"
+        webbrowser.get().open(url)
+        engine_speak("Deberías estar cerca de aquí, según google maps")    
+
+
+
+time.sleep(1)
+
+person_obj = person()
+asis_obj = asis()
+asis_obj.name = 'jarvis'
+person_obj.name = ""
